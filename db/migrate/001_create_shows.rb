fe4578b4075a4191658dbe74de2d41ba6e73c939
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-07-04 23:13:38 +0000

    Done.

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,12 @@
+class CreateShows < ActiveRecord::Migration[5.2]
+    def change
+        create_table :shows do |t|
+            t.string :name
+            t.string :day
+            t.string :season
+            t.string :network
+            t.integer :rating
+          end
+
+    end
+end
